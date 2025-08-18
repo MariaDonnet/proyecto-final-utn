31de6461910e8c2d2dc603a0dffba52bc81c4e99
Maria Soledad Donnet <msdonnet@hotmail.com>
2025-08-18 11:31:43 +0200

    Fix: modificación para el Footer quedo fijo al final de página aunque tenga poco contenido

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,10 @@
 import { Footer } from "./Footer"
 import { Header } from "./Header"
+import "../styles/components/Layout.css";
 
 const Layout = (props) => {
   return (
-    <div className={props.background}>
+    <div className={`layout-container ${props.background}`}>
       <Header />
       <main>
         {props.children}
